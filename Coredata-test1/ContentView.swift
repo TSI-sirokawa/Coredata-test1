e539a44b67471aef6a50f04eb473cd9ecd09a235
Kazunori Shirokawa <shirokawa@dream.com>
2022-11-09 09:32:11 +0900

    Tabview追加

--- Coredata-test1/ContentView.swift
+++ Coredata-test1/ContentView.swift
@@ -9,100 +9,32 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    /// 被管理オブジェクトコンテキスト（ManagedObjectContext）の取得
-    @Environment(\.managedObjectContext) private var context
-    @StateObject private var m_categorymodel = M_CategoryModel()
-    /// データ取得処理
-    @FetchRequest(
-        entity: M_CATEGORY.entity(),
-        sortDescriptors: [NSSortDescriptor(keyPath: \M_CATEGORY.updDateTime, ascending: true)],
-        predicate: nil
-    ) private var categories: FetchedResults<M_CATEGORY>
-    
-    @State private var searchText: String = ""
+    @State var tabSelection = 0
     
     var body: some View {
-        NavigationView {
-            /// 取得したデータをリスト表示
-            List {
-                ForEach(categories) { category in
-                    M_CategoryCardView(m_categoryModel: m_categorymodel, category: category)
-//                    VStack {
-//                        /// タスクの表示
-//                        //(destination: UpdTaskView(rec: categories.first!)){
-//                        NavigationLink(destination: AddTaskView()){
-//                            //HStack {
-//                            Image(systemName: category.checked ? "checkmark.circle.fill" : "circle")
-//                                .onTapGesture {
-//                                    category.checked.toggle()
-//                                    try? context.save()
-//                                }
-//                            Text("\(category.categoryCode!)")
-//                            Text("\(category.categoryName!)")
-//                            Spacer()
-//                            //}
-//                        }
-//
-//                        //                    /// タスクをタップでcheckedフラグを変更する
-//                        //                    .contentShape(Rectangle())
-//                    }
-                }
-                .onDelete(perform: deleteTasks)
-            }
-            .navigationTitle("部門リスト")
-            /// ツールバーの設定
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    EditButton()
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
+        TabView(selection: $tabSelection){
+            M_CategoryListView()
+                .tabItem {
+                    Image(systemName: "square.and.arrow.up")
+                    Text("部門")
+                }.tag(0)
 
-//                    NavigationLink(destination: AddTaskView()) {
-//                        Image(systemName: "plus")
-//                    }
-                    Button(action:
-                              {m_categorymodel.isNewData.toggle()
-                              }){
-                          Text("新規作成")
-                    }
-          //タップするとシートが開く
-                      .sheet(isPresented: $m_categorymodel.isNewData,
-                          content: {
-                          M_CategorySheetView(m_categoryModel: m_categorymodel)
-                      })
-                }
-            }
+            TabBView()
+                .tabItem {
+                    Image(systemName: "pencil.circle")
+                    Text("TabB")
+                }.tag(1)
+
         }
-        .navigationViewStyle(StackNavigationViewStyle())// iPhoneとiPadの見え方を同じにする
-        .searchable(text: $searchText, prompt: "検索")
-        // searchableの下に追加
-        .onChange(of: searchText) { newValue in
-            search(text: newValue)
-        }
-        
-    }
-    
-    private func search(text: String) {
-        if text.isEmpty {
-            categories.nsPredicate = nil // ①
-        } else {
-            let titlePredicate: NSPredicate = NSPredicate(format: "categoryName contains %@", text) // ②
-            //let contentPredicate: NSPredicate = NSPredicate(format: "content contains %@", text) // ③
-            //tasks.nsPredicate = NSCompoundPredicate(orPredicateWithSubpredicates: [titlePredicate, contentPredicate]) //　④
-            categories.nsPredicate = NSCompoundPredicate(orPredicateWithSubpredicates: [titlePredicate]) //　④
-        }
-    }
-    /// タスクの削除
-    /// - Parameter offsets: 要素番号のコレクション
-    func deleteTasks(offsets: IndexSet) {
-        for index in offsets {
-            context.delete(categories[index])
-        }
-        try? context.save()
     }
     
 }
 
+struct TabBView: View {
+    var body: some View {
+        Text("TabB")
+    }
+}
 /// タスク追加View
 struct AddTaskView: View {
     @Environment(\.managedObjectContext) private var context
